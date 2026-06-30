4458772c11afd26353e1f35eb17d10e407d5eefa
Milton Franco Ruiz <94766039+miltonfruiz@users.noreply.github.com>
2026-06-30 04:43:13 -0300

    Add backend/middleware/auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded;
+    next();
+  } catch (error) {
+    res.status(401).send({ mensaje: 'Por favor, inicie sesión para acceder a este recurso' });
+  }
+};
+
+module.exports = authMiddleware;
